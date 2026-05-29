76b820ac21b1409d3185ef6ac7eecbcf325d68d5
pranavasree <pranavareddy27@gmail.com>
2026-05-29 13:17:10 -0400

    UPDATE API for Accounts Microservice

--- phase2/accounts/src/main/java/com/pranava/accounts/controller/AccountsController.java
+++ phase2/accounts/src/main/java/com/pranava/accounts/controller/AccountsController.java
@@ -107,19 +107,19 @@ public class AccountsController {
 //            )
 //    }
 //    )
-//    @PutMapping("/update")
-//    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
-//        boolean isUpdated = iAccountsService.updateAccount(customerDto);
-//        if(isUpdated) {
-//            return ResponseEntity
-//                    .status(HttpStatus.OK)
-//                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
-//        }else{
-//            return ResponseEntity
-//                    .status(HttpStatus.EXPECTATION_FAILED)
-//                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
-//        }
-//    }
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+        }
+    }
 //
 //    @Operation(
 //            summary = "Delete Account & Customer Details REST API",
