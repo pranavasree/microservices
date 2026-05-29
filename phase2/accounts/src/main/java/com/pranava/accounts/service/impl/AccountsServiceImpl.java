99cee9c57fb9ff705d2f0c6a050b1279b4633c73
pranavasree <pranavareddy27@gmail.com>
2026-05-29 14:28:45 -0400

    update auidt/metadata fields for Accounts Microservice

--- phase2/accounts/src/main/java/com/pranava/accounts/service/impl/AccountsServiceImpl.java
+++ phase2/accounts/src/main/java/com/pranava/accounts/service/impl/AccountsServiceImpl.java
@@ -37,8 +37,8 @@ public class AccountsServiceImpl implements IAccountsService {
                     +customerDto.getMobileNumber());
         }
 
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
+//        customer.setCreatedAt(LocalDateTime.now());
+//        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -58,8 +58,8 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
 
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
+//        newAccount.setCreatedAt(LocalDateTime.now());
+//        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 
