66676e14b0c4444a814f654300ed37d740b125b1
pranavasree <pranavareddy27@gmail.com>
2026-05-31 13:24:53 -0400

    Reading Configurations using @Value annotation

---
+++ phase6/v1-springboot/accounts/src/main/java/com/pranava/accounts/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.pranava.accounts.repository;
+
+
+import com.pranava.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+}
