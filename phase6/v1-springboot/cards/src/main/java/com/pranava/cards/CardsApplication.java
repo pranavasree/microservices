8ee85ff2bfe16d04a6ef5abd951c2ed7075d1158
pranavasree <pranavareddy27@gmail.com>
2026-05-31 15:49:25 -0400

    Spring Boot Profile changes for cards and loans

--- phase6/v1-springboot/cards/src/main/java/com/pranava/cards/CardsApplication.java
+++ phase6/v1-springboot/cards/src/main/java/com/pranava/cards/CardsApplication.java
@@ -1,5 +1,6 @@
 package com.pranava.cards;
 
+import com.pranava.cards.dto.CardsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -14,6 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 /*@ComponentScans({ @ComponentScan("com.pranava.loans.controller") })
 @EnableJpaRepositories("com.pranava.loans.repository")
 @EntityScan("com.pranava.loans.model")*/
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Cards Microservice REST API Documentation",
